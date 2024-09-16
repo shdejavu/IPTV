6b58987112a03bedeecf92ab75b73a653832692e
shdejavu <weiyixia@iastate.edu>
2024-09-15 23:37:49 -0500

    Update combine_m3u.py

--- combine_m3u.py
+++ combine_m3u.py
@@ -4,7 +4,9 @@ import time
 # URLs for the two files in their repositories
 url_0 = 'https://iptv-org.github.io/iptv/index.m3u'
 url_00 = 'https://raw.githubusercontent.com/yuanzl77/IPTV/main/live.m3u'
-url_list =[ 'https://iptv-org.github.io/iptv/languages/zho.m3u','https://iptv-org.github.io/iptv/languages/jpn.m3u','https://iptv-org.github.io/iptv/languages/eng.m3u']
+url_list =['https://iptv-org.github.io/iptv/languages/zho.m3u',
+           'https://iptv-org.github.io/iptv/languages/jpn.m3u',
+           'https://iptv-org.github.io/iptv/languages/eng.m3u']
 
 # Threshold in KB/s. URLs slower than this will be removed.
 SPEED_THRESHOLD_KBPS = 150  # Example: 100 KB/s
